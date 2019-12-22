832448cd7975afacf4f18c63b9f1e173648ac6f4
Ievgenia-suprunenko <suprunenkoj@gmail.com>
2019-12-22 15:38:37 +0200

    Second commit

--- src/HelloFromUser.java
+++ src/HelloFromUser.java
@@ -1,2 +1,6 @@
 public class HelloFromUser {
+    public static void main(String[] args) {
+        System.out.println("Hello from user");
+        System.out.println("Hello from user second time");
+    }
 }
